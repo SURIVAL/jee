c74373f68feccc1ad828fe7aafcf7745f6827732
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-04 21:13:39 +0530

    number.c

---
+++ number.c
@@ -0,0 +1,21 @@
+# jee
+#include<stdio.h>
+void main()
+{
+int n, a=20 ,b=35,c=40;
+printf("enter the value ");
+scanf("%d",&n);
+if(a<c)
+{
+printf("a is big");
+}
+else if(a<b)
+{
+printf("b is big");
+}
+else if(c<a)
+{
+printf("c is big");
+}
+}
+
